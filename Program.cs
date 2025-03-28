bd33e8af06d3e5b23c0165409047587d23efe0c2
LuanGCMenezes <luangui2006@gmail.com>
2025-03-28 15:49:35 -0300

    Adicionando nome

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+string nome;
+string sobrenome;
+
+
+Console.Write("Digite o seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!.ToUpper();
+
+Console.WriteLine($"Nome Completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de Catálogo: {sobrenome}, {nome}");
+
